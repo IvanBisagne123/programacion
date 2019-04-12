7d151d6b7d79ad918d5700048920d3b9fa6d6be2
IvanBisagne123 <47418161+IvanBisagne123@users.noreply.github.com>
2019-04-12 12:23:10 -0300

    Add files via upload

---
+++ 4/12/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char nombre[50];
+    char apellido[50];
+    int edad;
+    int nota;
+}eAlumno
+
+int main()
+{
+    eAlumno unAlumno = {};
+
+
+
+
+
+    return 0;
+}
