752a0b677866c6bea4fd20dd6cac8bad4546ae34
IvanBisagne123 <47418161+IvanBisagne123@users.noreply.github.com>
2019-05-03 12:03:50 -0300

    Add files via upload

---
+++ Clase9-2.0/main.c
@@ -0,0 +1,16 @@
+/*
+  1. Todos los productos con su proveedor.
+  2. Por cada proveedor sus productos.
+  3. Por cada proveedor sus productos.
+  4. La suma de precios de los productos de cada proveedor.
+  5. El/Los proveedor/es que provee mas productos.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
